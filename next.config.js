4d7d7858fbb88cbd0426d9dc60e919edc1a6333a
Ashish Singh <ashishsingh.1899@gmail.com>
2026-02-15 14:02:26 +0530

    feat: Integrate Next.js bundle analyzer and update configuration file from MJS to JS.

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true"
+    ? require("@next/bundle-analyzer")({
+        enabled: true
+      })
+    : (config) => config;
+
+module.exports = withBundleAnalyzer(nextConfig);
